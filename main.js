ff512f01ddf30e7ed7a7378cf6a868bdf7e4a347
Jonathan <jonathan.j.curtis@outlook.com>
2023-01-09 14:19:19 -0600

    Complete game structure

--- main.js
+++ main.js
@@ -7,5 +7,29 @@ function getComputerChoice() {
 function game(playerSelection, computerSelection) {
     playerSelection = prompt('Rock, Paper, or Scissors?').toLowerCase();
     computerSelection = getComputerChoice();
-    return computerSelection;
+    if (playerSelection == '') {
+        message = console.log('Not interested in this game?')
+        return undefined;
+    } else {
+        message = console.log('Let\'s see who wins!');
+    }
+    console.log(`Player: ${playerSelection}`);
+    console.log(`Computer: ${computerSelection}`);
+
+    if (playerSelection == 'rock' & computerSelection == 'scissors') {
+        message = console.log('You win! Rock beats Scissors');
+    } else if (playerSelection == 'rock' & computerSelection == 'paper') {
+        message = console.log('You lose! Paper beats Rock');
+    } else if (playerSelection == 'paper' & computerSelection == 'rock') {
+        message = console.log('You win! Paper beats Rock');
+    } else if (playerSelection == 'paper' & computerSelection == 'scissors') {
+        message = console.log('You lose! Scissors beats Paper');
+    } else if (playerSelection == 'scissors' & computerSelection == 'paper') {
+        message = console.log('You win! Scissors beats Paper');
+    } else if (playerSelection == 'scissors' & computerSelection == 'rock') {
+        message = console.log('You lose! Rock beats Scissors');
+    } else {
+        message = console.log('Tie! Play again?')
+    }
+    return message;
 }
