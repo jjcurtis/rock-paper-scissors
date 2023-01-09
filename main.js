f891b9fb293aa00220594dce50deaa95105b02af
Jonathan <jonathan.j.curtis@outlook.com>
2023-01-09 12:00:48 -0600

    Set up variables for player/computer choices

--- main.js
+++ main.js
@@ -1,5 +1,11 @@
-const choice = ['rock', 'paper', 'scissors']
+const choice = ['rock', 'paper', 'scissors'];
 
 function getComputerChoice() {
     return choice[Math.floor(Math.random()*choice.length)];
+}
+
+function game(playerSelection, computerSelection) {
+    playerSelection = prompt('Rock, Paper, or Scissors?').toLowerCase();
+    computerSelection = getComputerChoice();
+    return computerSelection;
 }
