99047f9cdf2a74d6ce6b37d3a3b8565fa383d5c9
Jonathan <jonathan.j.curtis@outlook.com>
2023-01-10 12:17:16 -0600

    Create score variables and complete game

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 const choice = ['rock', 'paper', 'scissors'];
+
 function getComputerChoice() {
     return choice[Math.floor(Math.random()*choice.length)];
 }
+let playerScore = 0;
+let computerScore = 0;
 
-playerSelection = '';
-computerSelection = getComputerChoice();
-
-function playRound(playerSelection, computerSelection) {
+function playRound() {
     playerSelection = prompt('Rock, Paper, or Scissors?').toLowerCase();
     computerSelection = getComputerChoice();
     if (playerSelection == '') {
@@ -19,20 +19,31 @@ function playRound(playerSelection, computerSelection) {
     console.log(`Computer: ${computerSelection}`);
 
     if ((playerSelection == 'rock' & computerSelection == 'scissors') || (playerSelection == 'paper' & computerSelection == 'rock') || (playerSelection == 'scissors' & computerSelection == 'paper')) {
+        playerScore += 1;
         message = console.log(`You win! ${playerSelection} beats ${computerSelection}`);
     } else if ((playerSelection == 'rock' & computerSelection == 'paper') || (playerSelection == 'paper' & computerSelection == 'scissors') || (playerSelection == 'scissors' & computerSelection == 'rock')) {
+        computerScore += 1;
         message = console.log(`You lose! ${computerSelection} beats ${playerSelection}`);
     } else {
-        message = console.log('Tie! Play again?')
+        message = console.log('Tie!')
     }
     return message;
 }
 
 function game() {
     for (let i = 0; i < 5; i++) {
-        playerSelection = prompt('Rock, Paper, or Scissors?').toLowerCase();
-        computerSelection = getComputerChoice();
-        console.log(i);
-        // playRound();
+        console.log(`Round ${i + 1}`);
+        playRound();
+    }
+    
+    console.log(`FINAL SCORE:
+Player: ${playerScore} Computer: ${computerScore}`);
+
+    if (playerScore == computerScore) {
+        alert('Draw');
+    } else if (playerScore > computerScore) {
+        alert('You win!');
+    } else {
+        alert('You lose!')
     }
 }
